b906b4caeab16cb954b8a911ce9c7ea23923879e
Дмитрий Мельников <dima.dimix94@mail.ru>
2023-05-17 19:18:35 +0300

    config

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,42 @@
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
+
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+    //порядок вызываемых лоадеров имеет значение
+    const cssLoader = {
+            test: /\.s[ac]ss$/i,
+            use: [
+                isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+                /*"css-loader",*/
+                {
+                    loader: "css-loader",
+                    options: {
+                       modules: {
+                           //auto - автогенерация класса - обычные файлы будут обрабатываться как css
+                           auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                           //назначение имен классов
+                           //[path][name]__[local] - путь до компонента название класса и хэш
+                           // [hash:base64:8] - оставляем хэш с 8 симовали
+                           localIdentName: isDev
+                               ? '[path][name]__[local]--[hash:base64:5]'
+                               : '[hash:base64:8] '
+                       },
+                    }
+                },
+                "sass-loader",
+            ],
+        };
+
+    //Если не используем typescript - нужен babel-loader
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
+    return [
+        typescriptLoader,
+        cssLoader,
+    ]
+}
---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,32 @@
+import {BuildOptions} from "./types/config";
+import webpack from "webpack";
+import {buildPlugins} from "./buildPlugins";
+import {buildLoaders} from "./buildLoaders";
+import {buildResolvers} from "./buildResolvers";
+import {buildDevServer} from "./buildDevServer";
+
+export function buildWebpackConfig (options: BuildOptions): webpack.Configuration {
+
+    const { mode, paths, isDev } = options;
+
+    return {
+        mode,
+        /*  entry: {
+            //много entry (файлов)
+            RANDOM: path.resolve(__dirname, 'src', 'index.ts'), // RANDOM - название файла
+          }, //__dirname - папка в которой в данные момент находимся, далее участки пути*/
+        entry: paths.entry,
+        output: {
+            filename: "[name].[contenthash].js", //[name] - шаблон, contenthash - для генерации id
+            path: paths.build,
+            clean: true, //чистка предыдущих файлов
+        },
+        plugins: buildPlugins(options),
+        module: {
+            rules: buildLoaders(options),
+        },
+        resolve: buildResolvers(),
+        devtool: isDev ? 'inline-source-map' : undefined, //помогает например с поиском ошибки в коде
+        devServer: isDev ? buildDevServer(options) : undefined
+    }
+}
