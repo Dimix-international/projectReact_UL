ead3a8fc2d7097dcb87ecdcafc17453327fc8e63
Дмитрий Мельников <dima.dimix94@mail.ru>
2023-05-20 09:15:18 +0300

    hot reolad webpack

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import { BuildOptions } from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 
 export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
     return [
@@ -16,6 +17,8 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
         //с помощью его в приложении можно прокидывать глобальные переменные
         new webpack.DefinePlugin({
             __IS__DEV__: JSON.stringify(isDev)
-        })
+        }),
+        new ReactRefreshPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
     ]
 }
