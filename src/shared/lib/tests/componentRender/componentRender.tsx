55d7d06d235da996dc2caae9012769f6f9c50440
Дмитрий Мельников <dima.dimix94@mail.ru>
2023-06-22 23:22:47 +0300

    check

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -5,21 +5,12 @@ import i18nForTests from 'shared/config/i18n/i18nForTests';
 import { MemoryRouter } from 'react-router-dom';
 import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
 import { ReducersMapObject } from '@reduxjs/toolkit';
-import ReactDOM from 'react-dom/client';
 
 export interface componentRenderOptions {
     route?: string;
     initialState?: DeepPartial<StateSchema>;
     asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>
 }
-
-const container = document.getElementById('root');
-
-if (!container) {
-    throw new Error('Контейнер root не найден. НЕ удалось вмонтировать реакт приложение');
-}
-
-const root = ReactDOM.createRoot(container);
 
 export function componentRender(component: ReactNode, options: componentRenderOptions = {}) {
     const {
@@ -28,7 +19,7 @@ export function componentRender(component: ReactNode, options: componentRenderOp
         asyncReducers,
     } = options;
 
-    return root.render(
+    return render(
         <MemoryRouter initialEntries={[route]}>
             <StoreProvider asyncReducers={asyncReducers} initialState={initialState}>
                 <I18nextProvider i18n={i18nForTests}>
